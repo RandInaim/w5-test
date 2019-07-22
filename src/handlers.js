70a45081ebe45ee61f20a5a287b0190bb4185802
Rand Inaim <rand.inaim95@gmail.com>
2019-07-23 01:28:06 +0300

    solution

--- src/handlers.js
+++ src/handlers.js
@@ -1,50 +1,62 @@
 const fs = require('fs');
 const path = require('path');
+const request = require('request');
+require('env2')('.env');
 
 const handleHomeRoute = (request, response) => {
-  const filePath = path.join(__dirname, '..', 'public', 'index.html');
-  fs.readFile(filePath, (error, file) => {
-    if (error) {
-      console.log(error);
-      response.writeHead(500, {'Content-Type': 'text/html'});
-      response.end("<h1>Sorry, we've had a problem on our end</h1>");
-    } else {
-      response.writeHead(200, {'Content-Type': 'text/html'});
-      response.end(file);
-    }
-  });
+	const filePath = path.join(__dirname, '..', 'public', 'index.html');
+	fs.readFile(filePath, (error, file) => {
+		if (error) {
+			console.log(error);
+			response.writeHead(500, { 'Content-Type': 'text/html' });
+			response.end("<h1>Sorry, we've had a problem on our end</h1>");
+		} else {
+			response.writeHead(200, { 'Content-Type': 'text/html' });
+			response.end(file);
+		}
+	});
 };
 
 const handlePublic = (request, response, url) => {
-  const extension = url.split('.')[1];
-  const extensionType = {
-    html: 'text/html',
-    css: 'text/css',
-    js: 'application/javascript',
-    ico: 'image/x-icon',
-    jpg: 'image/jpeg',
-    png: 'image/png'
-  };
-  const filePath = path.join(__dirname, '..', url);
+	const extension = url.split('.')[1];
+	const extensionType = {
+		html: 'text/html',
+		css: 'text/css',
+		js: 'application/javascript',
+		ico: 'image/x-icon',
+		jpg: 'image/jpeg',
+		png: 'image/png'
+	};
+	const filePath = path.join(__dirname, '..', url);
 
-  fs.readFile(filePath, (error, file) => {
-    if (error) {
-      console.log(error);
-      response.writeHead(404, {'Content-Type': 'text/html'});
-      response.end('<h1>404 file not found</h1>');
-    } else {
-      response.writeHead(200, {'Content-Type': extensionType[extension]});
-      response.end(file);
-    }
-  });
+	fs.readFile(filePath, (error, file) => {
+		if (error) {
+			console.log(error);
+			response.writeHead(404, { 'Content-Type': 'text/html' });
+			response.end('<h1>404 file not found</h1>');
+		} else {
+			response.writeHead(200, { 'Content-Type': extensionType[extension] });
+			response.end(file);
+		}
+	});
 };
 
-const handleAPI = (request, response) => {
-  // add code here!
-}
+const handleAPI = (req, response) => {
+	// add code here!
+	console.log(process.env.API_KEY);
+	const url = `http://www.omdbapi.com/?s=star-wars&apikey=${process.env.API_KEY}`;
+	request(url, (error, res, body) => {
+		if (error) {
+			response.writeHead(404, { 'Content-Type': 'text/html' });
+			response.end('<h1>Try again</h1>');
+		}
+		response.writeHead(200, { 'Content-Type': 'application/JSON' });
+		response.end(body);
+	});
+};
 
 module.exports = {
-  handleHomeRoute,
-  handlePublic,
-  handleAPI
+	handleHomeRoute,
+	handlePublic,
+	handleAPI
 };
--- src/server.js
+++ src/server.js
@@ -1,9 +1,8 @@
 const http = require('http');
-const router = require('./router')
+const router = require('./router');
 
-const port = 4000;
+const port = 3000;
 
 const server = http.createServer(router);
 
-server.listen(port,  () => console.log(`server up and running on localhost:${port}`));
-
+server.listen(port, () => console.log(`server up and running on localhost:${port}`));
